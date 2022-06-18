915593020fc7e91f0c89d85cde81e87a8b0498d2
Alex Morales <44649358+msalemor@users.noreply.github.com>
2022-06-18 11:27:09 -0400

    Update build.sh

--- src/mandelbrot-rs/build.sh
+++ src/mandelbrot-rs/build.sh
@@ -1,5 +1,7 @@
+# Build the WASM file
 cargo build --release --target=wasm32-unknown-unknown
 export rs_build="target/wasm32-unknown-unknown/release/"
 export target="../mandelbrot-py-wasm/"
+# Copy the WASM file to the python folder
 cp ${rs_build}mandelbrot_rs.wasm ${target}mandelbrot.wasm
 
